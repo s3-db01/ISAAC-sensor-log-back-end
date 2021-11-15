a9538deac379316a3cbb7ff2c3c05e555a59af63
Jules <jules.houben@live.nl>
2021-11-15 13:56:09 +0100

    Reverting table name change

--- app/migrations/20211102124637-create-sensor-log.js
+++ app/migrations/20211102124637-create-sensor-log.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Sensor_logs', {
+    await queryInterface.createTable('SensorLogs', {
       id:{
         allowNull: false,
         autoIncrement: true,
@@ -32,6 +32,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Sensor_logs');
+    await queryInterface.dropTable('SensorLogs');
   }
 };

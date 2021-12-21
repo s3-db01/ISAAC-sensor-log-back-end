23e26ba6ae2bde3b98710458dab7ab0999350c6a
Jules <jules.houben@live.nl>
2021-12-21 12:53:26 +0100

    Updated CORS value

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:3005",
+  credentials: true
 };
 
 app.use(cors(corsOptions));
